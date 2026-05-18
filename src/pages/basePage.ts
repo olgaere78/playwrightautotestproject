bfce150ca56b94959c7300fadd886ca638171b6a
Olga Ieromenko <olgaere78@gmail.com>
2026-05-18 12:36:10 +0300

    Format code with Prettier

--- src/pages/basePage.ts
+++ src/pages/basePage.ts
@@ -1,29 +1,29 @@
 import { Page, expect } from '@playwright/test';
 
 export class BasePage {
-    constructor(protected page: Page) { }
+  constructor(protected page: Page) {}
 
-    goto = async (path: string = '') => {
-        await this.page.goto(path);
-    };
+  goto = async (path: string = '') => {
+    await this.page.goto(path);
+  };
 
-    reload = async () => {
-        await this.page.reload();
-    };
+  reload = async () => {
+    await this.page.reload();
+  };
 
-    goBack = async () => {
-        await this.page.goBack();
-    };
+  goBack = async () => {
+    await this.page.goBack();
+  };
 
-    assertUrlIs = (url: string) => {
-        expect(this.page.url()).toContain(url);
-    };
+  assertUrlIs = (url: string) => {
+    expect(this.page.url()).toContain(url);
+  };
 
-    waitForTitle = async (title: string | RegExp) => {
-        await expect(this.page).toHaveTitle(title);
-    };
+  waitForTitle = async (title: string | RegExp) => {
+    await expect(this.page).toHaveTitle(title);
+  };
 
-    getCurrentUrl = () => {
-        return this.page.url();
-    };
+  getCurrentUrl = () => {
+    return this.page.url();
+  };
 }
